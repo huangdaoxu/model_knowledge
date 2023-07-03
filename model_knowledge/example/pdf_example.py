3d3a246444cb9333b13a13e9774d54a2d93e828b
dawsonhuang <382619605@qq.com>
2023-07-03 18:57:06 +0800

    add pdf image extraction

--- model_knowledge/example/pdf_example.py
+++ model_knowledge/example/pdf_example.py
@@ -3,7 +3,7 @@ from model_knowledge.pdf.image import extract_images
 
 
 if __name__ == "__main__":
-    images = extract_images(file='./example.pdf')
+    images = extract_images(file='/Users/dawsonhuang/Downloads/330B88E8457B6271E628EDB2CB3_016E9BC2_1E7F67.pdf')
     for image in images:
         a = ocr(image)
         print(a)
--- model_knowledge/pdf/image.py
+++ model_knowledge/pdf/image.py
@@ -11,19 +11,12 @@ def extract_images(file):
     :param file: file absolute path
     :return: image binary io iterator
     """
-    img_count = 0
     with fitz.open(file) as pdf:
-        len_xref = pdf.xref_length()
-        logger.debug("文件名:{}, 页数: {}, 对象: {}".format(file, len(pdf), len_xref - 1))
-
         for page in pdf:
             try:
-                img_count += 1
-                images = page.get_images()
-                for xref in images:
-                    xref = xref[0]
-                    img = pdf.extract_image(xref)
-                    yield img["image"]
+                # 扩大像素2倍
+                mat = fitz.Matrix(2.0, 2.0)
+                yield page.get_pixmap(matrix=mat).tobytes()
             except Exception as e:
                 logger.exception(e)
 
